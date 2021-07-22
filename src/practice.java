60f51ef20bbdb5b13cceefb7dd0dce86e238682f
Sharanna <rsharanna@gmail.com>
2021-07-22 10:04:24 -0400

    First Commit

---
+++ src/practice.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class practice {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+
+        System.out.println("I need help!");
+        String response = userInput.nextLine();
+        System.out.println (response + " You sure do!");
+        System.out.println(response + "Keep practicing!");
+
+
+
+
+
+    }
+}
